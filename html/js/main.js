d41ede686ed2fda0ae375d83a51301fb0efce3a9
Jakirservice22 <89473136+Jakirservice22@users.noreply.github.com>
2021-08-24 22:13:59 +0600

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,9 @@
+//Bs Slider
+
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanmenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
+  });
